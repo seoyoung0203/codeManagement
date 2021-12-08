824ed3d75fd49b85d7ca843ec23817c2401fe2c8
seoyoung <dlwlrma0203@gmail.com>
2021-12-08 16:42:10 +0900

    자식 코드 조회 service 생성

--- src/codes/codes.service.ts
+++ src/codes/codes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { CodeInfo } from '../entity/CodeInfo';
-import { getConnection } from 'typeorm';
+import { getConnection, getManager } from 'typeorm';
 import { CreateCode, UpdateCode } from './code.interface';
 
 @Injectable()
@@ -12,6 +12,33 @@ export class CodesService {
       .into(CodeInfo)
       .values([codes])
       .execute();
+  }
+
+  async getMyDepth(code: string) {
+    const entityManager = getManager();
+    const depth = await entityManager.query(`
+      select myDepth from code_info where code = "${code}";
+    `);
+    return depth[0];
+  }
+
+  async getChildsCodeInfo(code: string) {
+    const entityManager = getManager();
+    const data = await entityManager.query(
+      `
+      select 
+        my.code as myCodey, 
+        my.name as myCodeName,
+        child.code as childCode, 
+        child.name as childCodeName
+      from code_info as my 
+      inner join code_info as child on my.preDepthCode = child.code 
+      where my.code = ?
+      order by child.sortNum, my.sortNum;
+    `,
+      [code],
+    );
+    return data;
   }
 
   async update(id: number, updatedData: UpdateCode) {
