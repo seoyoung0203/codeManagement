6a37152e62618970a6c6bab5b97dbd6625bc525b
seoyoung <dlwlrma0203@gmail.com>
2021-12-08 09:23:11 +0900

    nestjs/config 사용하여 환경변수 관리 및 .env.example 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CodesController } from './codes/codes.controller';
 import { CodesService } from './codes/codes.service';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      synchronize: false,
+    }),
+  ],
   controllers: [CodesController],
   providers: [CodesService],
 })
