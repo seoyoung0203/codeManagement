5569217bed649cfa57f58a9ad2758b8c6e49c01d
seoyoung <dlwlrma0203@gmail.com>
2021-12-08 18:07:59 +0900

    ormconfig.js 로 관리

---
+++ ormconfig.js
@@ -0,0 +1,15 @@
+module.exports = {
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [],
+  synchronize: true,
+  entities: ['/src/entity/*'],
+  migrations: ['migration/*'],
+  cli: {
+    migrationsDir: 'migration',
+  },
+};
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,23 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CodesController } from './codes/codes.controller';
 import { CodesService } from './codes/codes.service';
-import { CodeInfo } from './entity/CodeInfo';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: 3306,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      entities: [CodeInfo],
-      synchronize: false,
-    }),
+    TypeOrmModule.forRoot(),
   ],
   controllers: [CodesController],
   providers: [CodesService],
